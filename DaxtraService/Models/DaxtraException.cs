dd9caa08274acbd3c39d89f83146ad50f010af2a
KeithHenry <keith.henry@evolutionjobs.co.uk>
2018-03-26 18:52:59 +0100

    Tidied up exception

--- DaxtraService/Models/DaxtraException.cs
+++ DaxtraService/Models/DaxtraException.cs
@@ -1,14 +1,14 @@
-﻿namespace Evolution.Daxtra.Models
-{using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿namespace Evolution.Daxtra
+{
+    using System;
     using System.Net;
-    using System.Threading.Tasks;
 
-    public class DaxtraException: Exception
+    /// <summary>Exception thrown for issues with the Daxtra service</summary>
+    public class DaxtraException : Exception
     {
         public DaxtraException(string message, int code, HttpStatusCode httpStatus, string body) :
-            base(message) {
+            base(message)
+        {
             this.Data.Add("json-response", body);
             this.Code = code;
             this.HttpStatus = httpStatus;
@@ -21,8 +21,10 @@ using System.Linq;
             this.HttpStatus = HttpStatusCode.OK;
         }
 
+        /// <summary>Get or set the HTTP status code of the error.</summary>
         public HttpStatusCode HttpStatus { get; set; }
 
+        /// <summary>Get or set the error code recieved from Daxtra.</summary>
         public int Code { get; set; }
     }
 }
